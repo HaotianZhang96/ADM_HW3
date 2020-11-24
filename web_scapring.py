cadae475d7c77b97c2b6e224141408620c4d41a9
alessandradellafazia <alessdellafazia@gmail.com>
2020-11-24 10:21:47 +0100

    point 1.2 creations of directories and pages storing

--- web_scapring.py
+++ web_scapring.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 home_page = 'https://www.goodreads.com'
 
@@ -25,9 +26,45 @@ def get_url_list(filename):
     with open(filename, 'r') as inputFile:
         urls = inputFile.read().splitlines()
         return urls
-        
+
+
+def save_page_in_folder(page_urls, start_page_number, path):
+    
+    page_directory_list = [] #list of paths
+
+    #create all the directory 
+    for page_number in range(len(page_urls)):
+        page_directory = os.path.join('data','page_' +  str(start_page_number + page_number + 1))
+        page_directory_list.append(page_directory)
+        if not os.path.exists(page_directory):
+            os.makedirs(page_directory)
+
+    #get all the books html and save in folders 
+    for page_number in range(len(page_urls)):
+        book_list = page_urls[page_number]
+        for book_number in range(len(book_list)):
+            book_file_path = os.path.join(page_directory_list[page_number], 'content_' +
+             str(100*(start_page_number + page_number) + 1 + book_number) + '.txt')
+
+            book_web = requests.get(book_list[book_number])
+            with open(book_file_path, 'w', encoding="utf-8") as output_file:
+                output_file.write(book_web.text)
+
 
 if __name__ == "__main__":
     #save_books_url('bookurls.txt') commented because the urls have already been stored
-    urls = get_url_list('bookurls.txt') 
+    source_path = os.path.dirname(__file__) 
     
+    all_urls = get_url_list(os.path.join(source_path, 'bookurls.txt'))
+    
+    
+    pages_urls = [all_urls[i:i + 100] for i in range(0, len(all_urls), 100)] # split list in 300 sublists of 100 items
+    
+    #save_page_in_folder(pages_urls[1], 1, source_path)
+    
+
+    #save_page_in_folder(page_urls[:100], 0, source_path) #first 100 pages
+    
+    save_page_in_folder(pages_urls[100:200], 100, source_path)
+    
+    #save_page_in_folder(page_urls[200:300], 200, source_path))
