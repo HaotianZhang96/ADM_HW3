817fb9590a229785e38fc1d387b99a5c764d5bf4
alessandradellafazia <alessdellafazia@gmail.com>
2020-11-25 17:55:55 +0100

    delete unuseless file

--- prove.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url = 'https://www.goodreads.com/book/show/2767052-the-hunger-games'
-
-page = requests.get(url).text
-
-soup = BeautifulSoup(page)
-print(soup.prettify())
